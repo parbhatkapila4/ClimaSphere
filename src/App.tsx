16b2459077a5ebe685eb77540c523d600db3a88c
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-08-19 21:10:06 +0530

    Boiler Plate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App (){
+  return(
+    <div>
+      "Hey there"
+      <Button variant="destructive">Ready</Button>
+    </div>
+  )
+}
+export default App;
