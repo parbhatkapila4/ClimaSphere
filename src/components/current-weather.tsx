255d1b7b2b0fa755cbc9cdf873e4fff8f6f6185e
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-08-20 22:18:42 +0530

    Icon Display

--- src/components/current-weather.tsx
+++ src/components/current-weather.tsx
@@ -1,6 +1,6 @@
 import type { GeocodingResponse, WeatherData } from "@/api/types";
 import { Card, CardContent } from "./ui/card";
-import { ArrowDown } from "lucide-react";
+import { ArrowDown, ArrowUp, Droplets, Wind } from "lucide-react";
 
 interface CurrentWeatherProps {
   data: WeatherData;
@@ -49,7 +49,41 @@ const CurrentWeather = ({ data, locationName }: CurrentWeatherProps) => {
                     <ArrowDown className="h-3 w-3" />
                     {formatTemp(temp_min)}
                   </span>
+                  <span className="flex items-center gap-1 text-red-500">
+                    <ArrowUp className="h-3 w-3" />
+                    {formatTemp(temp_max)}
+                  </span>
                 </div>
+              </div>
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex items-center gap-2">
+                <Droplets className="h-4 w-4 text-blue-500" />
+                <div className="space-y-0.5">
+                  <p className="text-sm font-medium">Humidity</p>
+                  <p className="text-sm text-muted-foreground">{humidity}%</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-2">
+                <Wind className="h-4 w-4 text-blue-500" />
+                <div className="space-y-0.5">
+                  <p className="text-sm font-medium">Wind Speed</p>
+                  <p className="text-sm text-muted-foreground">{speed} m/s</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div className="relative flex aspect-square w-full max-w-[200px] items-center justify-center">
+              <img
+                src={`https://openweathermap.org/img/wn/${currentWeather.icon}@4x.png`}
+              />
+              <div className="absolute bottom-0 text-center">
+                <p className="text-sm font-medium capitalize">
+                  {currentWeather.description}
+                </p>
               </div>
             </div>
           </div>
